0ed17efa72998ab48a3b2d39f8905296295dd226
Mohamed Abdeen <3bdenoz1000@gmail.com>
2025-10-24 15:16:22 +0300

    Update script.js

--- Movie Review Website Project/script.js
+++ Movie Review Website Project/script.js
@@ -1,1 +1,39 @@
+const movies = [
+  { title: "Avatar", year: 2009, genre: "Action" },
+  { title: "Titanic", year: 1997, genre: "Drama" },
+  { title: "The Avengers", year: 2012, genre: "Action" },
+  { title: "Frozen", year: 2013, genre: "Animation" },
+  { title: "The Godfather", year: 1972, genre: "Crime" },
+  { title: "John Wick", year: 2014, genre: "Action" },
+  { title: "Cars", year: 2006, genre: "Animation" },
+  { title: "Scarface", year: 1983, genre: "Crime" },
+  { title: "Toy Story", year: 1995, genre: "Animation" },
+  { title: "Interstellar", year: 2014, genre: "Sci-Fi" },
+  { title: "Kung Fu Panda", year: 2008, genre: "Animation" },
+  { title: "Get Out", year: 2017, genre: "Horror" },
+  { title: "Sherlock Holmes", year: 2009, genre: "Mystery" },
+  { title: "The Conjuring", year: 2013, genre: "Horror" },
+  { title: "Fast & Furious 7", year: 2015, genre: "Action" },
+  { title: "Harry Potter and the Sorcerer's Stone", year: 2001, genre: "Fantasy" },
+  { title: "The Dark Knight", year: 2008, genre: "Action" },
+  { title: "Inception", year: 2010, genre: "Sci-Fi" },
+  { title: "Joker", year: 2019, genre: "Drama" }
+];
 
+function showMovies(list) {
+  let container = document.getElementById("movieContainer");
+  container.innerHTML = "";
+
+  for (let i = 0; i < list.length; i++) {
+    container.innerHTML += `
+      <div class="movie">
+        <h3>${list[i].title}</h3>
+        <p>${list[i].genre} - ${list[i].year}</p>
+      </div>
+    `;
+  }
+}
+
+window.onload = function() {
+  showMovies(movies);
+};
